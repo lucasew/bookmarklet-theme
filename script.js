b833478dbde0a3e137cc7ab70882f66726fab011
lucasew <lucas59356@gmail.com>
2021-12-29 17:52:05 -0300

    if link#theme is a css and exists just reuse it

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 (function () {
     const url = new URL(document.currentScript.src)
-    const cssNode = document.createElement('link');
-    const buttonNode = document.createElement('button')
-
     let cssTheme = url.searchParams.get('theme') || 'white'
     const cssDefaultTheme = url.searchParams.get('defautTheme') || cssTheme
     const cssDefaultDarkTheme = url.searchParams.get('defautDarkTheme') || 'dark'
     const localstorageKey = url.searchParams.get('localstorageKey') || 'theme'
+
+    const buttonNode = document.createElement('button')
+    let cssNode = null
+    if (!cssNode) {
+        const themeNode = document.getElementById('theme')
+        if (themeNode) {
+            if (themeNode.nodeName == "LINK" && themeNode.rel == "stylesheet") {
+                cssNode = themeNode
+            }
+        }
+    }
+    if (!cssNode) {
+        let cssNode = document.createElement('link');
+        document.head.appendChild(cssNode)
+    }
+
     cssNode.id = "css";
     cssNode.rel = "stylesheet";
     cssNode.href = "https://unpkg.com/sakura.css/css/sakura.css";
     cssNode.type = "text/css";
-    document.head.appendChild(cssNode)
     const themes = {
         'white': "https://unpkg.com/sakura.css/css/sakura.css",
         'dark': "https://unpkg.com/sakura.css/css/sakura-dark.css"
