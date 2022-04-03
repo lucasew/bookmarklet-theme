7ac734e75be272ca994d6adf702101a5200ab830
lucasew <lucas59356@gmail.com>
2022-04-03 14:04:16 -0300

    responsiveness work
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 (function () {
+    const root = document.querySelector(':root')
     const url = new URL(document.currentScript.src)
     let cssTheme = url.searchParams.get('theme') || 'white'
     const cssDefaultTheme = url.searchParams.get('defautTheme') || cssTheme
     const cssDefaultDarkTheme = url.searchParams.get('defautDarkTheme') || 'dark'
     const localstorageKey = url.searchParams.get('localstorageKey') || 'theme'
 
-    let fontSizePx = parseInt(url.searchParams.get('fontsize')) || parseInt(window.getComputedStyle(document.body).fontSize) || 18
+    let fontSizeRem = parseInt(url.searchParams.get('fontsize')) || 1
 
     const buttonThemeToggle = document.createElement('button')
     const buttonArea = document.createElement('div')
@@ -38,10 +39,11 @@
         const theme = getSelectedThemeName()
         buttonThemeToggle.innerHTML = theme + "<sub> </sub>"
         cssNode.href = themes[theme] || themes[cssDefaultTheme]
-        document.body.style.fontSize = String(fontSizePx) + "px"
+        const size = String(fontSizeRem)
+        root.style.fontSize = `calc(16px + ${size}px)`
     }
     function changeFontSize(diff) {
-        fontSizePx = fontSizePx + diff
+        fontSizeRem = fontSizeRem + diff
         triggerThemeChange()
     }
     function toggleTheme() {
@@ -55,20 +57,28 @@
     }
     triggerThemeChange()
     buttonThemeToggle.onclick = toggleTheme
+    buttonThemeToggle.style.height = "0.5in"
+    buttonThemeToggle.style.width = "calc(100vw / 3)"
     
     const buttonIncreaseFont = document.createElement('button')
     buttonIncreaseFont.onclick = () => changeFontSize(1)
     buttonIncreaseFont.innerHTML = 'A<sub>+</sub>'
+    buttonIncreaseFont.style.height = "0.5in"
+    buttonIncreaseFont.style.width = "calc(100vw / 3)"
 
     const buttonDecreaseFont = document.createElement('button')
     buttonDecreaseFont.onclick = () => changeFontSize(-1)
     buttonDecreaseFont.innerHTML = 'A<sub>-</sub>'
+    buttonDecreaseFont.style.height = "0.5in"
+    buttonDecreaseFont.style.width = "calc(100vw / 3)"
 
     buttonArea.style.position = "fixed"
     buttonArea.style.bottom = 0
-    buttonArea.style.right = 0
+    buttonArea.style.left = 0
+    buttonArea.style.lineHeight = 0
+    buttonArea.style.fontSize = 0
     buttonArea.appendChild(buttonIncreaseFont)
-    buttonArea.appendChild(buttonDecreaseFont)
     buttonArea.appendChild(buttonThemeToggle)
+    buttonArea.appendChild(buttonDecreaseFont)
     document.body.appendChild(buttonArea)
 })()
